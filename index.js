11d7091e1395fedb53a558c579756e80bf819d2b
zilviazz <zilvia524@gmail.com>
2023-02-20 09:57:14 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//mengambil id dari button
+document.getElementById("getPosts").addEventListener("click", getPost)
+
+function getPost(){
+    fetch("https://digimon-api.vercel.app/api/digimon")
+    .then((response) => response.json())
+    .then((data) =>{
+        let output = "";
+        console.log("data",data)
+
+        data.forEach((post) => {
+            output += `
+            <div class="card-item">
+                <h3>${post.name}</h3>
+                <img src="${post.img}" class="img-item"/>
+                <h3>${post.img}</h3>
+                <h3>${post.level}</h3>
+            </div>
+            `;
+        });
+
+        document.getElementById("output").innerHTML = output;
+    })
+
+    .catch((err) => console.log(err));
+}
